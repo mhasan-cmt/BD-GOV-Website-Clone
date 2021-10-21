3647820a26aa209bad20171c98d9021dca669e07
Mahmudul Hasan Shafin <mhshafin220@gmail.com>
2021-10-21 22:16:27 +0600

    tab section modified and finished

--- js/app.js
+++ js/app.js
@@ -1,25 +1,26 @@
-$(document).ready(()=>{
+$(document).ready(() => {
     $('.slider').slick({
-    arrows:false,
-    infinite: true,
-    speed: 500,
-    fade: true,
-    cssEase: 'linear',
-    autoplay: true,
-    autoplaySpeed: 2000,
+        arrows: false,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear',
+        autoplay: true,
+        autoplaySpeed: 2000,
     });
 
+
     //Sub menu
-    $('.menu').hover(()=>{
-        
+    $('.menu').hover(() => {
+
         $('.sub-menu-wrapper').toggle();
     });
 
     // Tabs switcher
-    $('.tab-section .tab-item-wrapper .tab-item a').on('click',function(e){
-        var currentAttrValue=$(this).attr('href');
+    $('.tab-section .tab-item-wrapper .tab-item a').on('click', function(e) {
+        var currentAttrValue = $(this).attr('href');
 
-        $('.tab-section '+currentAttrValue).fadeIn(400).siblings().hide();
+        $('.tab-section ' + currentAttrValue).fadeIn(400).siblings().hide();
 
         $(this).parent('div').addClass('active').siblings().removeClass('active');
         e.preventDefault();
